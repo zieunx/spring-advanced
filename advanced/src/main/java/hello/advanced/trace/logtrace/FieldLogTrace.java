29b7cb229b032e6a0f8138a71df9c6978475628f
zieunx <wldms_1217@naver.com>
2021-12-08 23:34:22 +0900

    fix(rename): FieldLogTrace.java

--- advanced/src/main/java/hello/advanced/trace/logtrace/FieldLogTrace.java
+++ advanced/src/main/java/hello/advanced/trace/logtrace/FieldLogTrace.java
@@ -5,7 +5,7 @@ import hello.advanced.trace.TraceStatus;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class FleidLogTrace implements LogTrace {
+public class FieldLogTrace implements LogTrace {
 
     private static final String START_PREFIX = "-->";
     private static final String COMPLETE_PREFIX = "<--";
