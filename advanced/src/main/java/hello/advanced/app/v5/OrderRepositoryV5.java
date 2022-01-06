80f141f4672f120ea12e6589006b4442ed812de7
zieunx <wldms_1217@naver.com>
2022-01-06 22:27:32 +0900

    feat(템플릿콜백패턴): 서비스에 적용하기

---
+++ advanced/src/main/java/hello/advanced/app/v5/OrderRepositoryV5.java
@@ -0,0 +1,34 @@
+package hello.advanced.app.v5;
+
+import hello.advanced.trace.callback.TraceTemplate;
+import hello.advanced.trace.logtrace.LogTrace;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OrderRepositoryV5 {
+	private final TraceTemplate template;
+
+	public OrderRepositoryV5(LogTrace logTrace) {
+		this.template = new TraceTemplate(logTrace);
+
+	}
+
+	public void save(String itemId) {
+		template.execute("OrderRepository.save()", () -> {
+			// 저장 로직
+			if (itemId.equals("ex")) {
+				throw new IllegalStateException("예외발생!");
+			}
+			sleep(1000);
+			return null;
+		});
+	}
+
+	private void sleep(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
