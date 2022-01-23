be892a665c34a99a0e28bb39cec0d10f1a3eb02c
zieunx <wldms_1217@naver.com>
2022-01-24 00:26:47 +0900

    feat(데코레이터패턴): 두 가지 데코레이터(응답값변형, 시간측정)을 적용한 데코레이터패턴 Test

--- proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
 import hello.proxy.pureproxy.decorator.code.MessageDecorator;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.TimeDecorator;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -22,6 +23,16 @@ public class DecoratorPatternTest {
 		RealComponent realComponent = new RealComponent();
 		MessageDecorator messageDecorator = new MessageDecorator(realComponent);
 		DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+
+		client.execute();
+	}
+
+	@Test
+	void decorator2() {
+		RealComponent realComponent = new RealComponent();
+		MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+		TimeDecorator timeDecorator = new TimeDecorator(messageDecorator);
+		DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
 
 		client.execute();
 	}
