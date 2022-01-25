96c2bd2f0b34ba652d32f2fd6c3b65d1f0b94a5e
zieunx <wldms_1217@naver.com>
2022-01-25 21:46:10 +0900

    feat(구채클래스기반프록시): 인터페이스 없이 클래스기반 프록시

---
+++ proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
@@ -0,0 +1,25 @@
+package hello.proxy.pureproxy.concreteproxy;
+
+import org.junit.jupiter.api.Test;
+
+import hello.proxy.pureproxy.concreteproxy.code.ConcreteLogic;
+import hello.proxy.pureproxy.concreteproxy.code.ConcreteClient;
+import hello.proxy.pureproxy.concreteproxy.code.TimeProxy;
+
+public class ConcreteProxyTest {
+
+	@Test
+	void noProxy() {
+		ConcreteLogic concreteLogic = new ConcreteLogic();
+		ConcreteClient concreteClient = new ConcreteClient(concreteLogic);
+		concreteClient.execute();
+	}
+
+	@Test
+	void addProxy() {
+		ConcreteLogic concreteLogic = new ConcreteLogic();
+		TimeProxy timeProxy = new TimeProxy(concreteLogic);
+		ConcreteClient concreteClient = new ConcreteClient(timeProxy);
+		concreteClient.execute();
+	}
+}
