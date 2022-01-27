4c96b153eb6aee1d73aa88788d95b23feab63415
zieunx <wldms_1217@naver.com>
2022-01-28 00:00:22 +0900

    feat(구체클래스프록시): config 적용

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,5 +1,6 @@
 package hello.proxy;
 
+import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +14,8 @@ import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 
 // @Import({AppV1Config.class, AppV2Config.class})
-@Import(InterfaceProxyConfig.class)
+//@Import(InterfaceProxyConfig.class)
+@Import(ConcreteProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의 예제 테스트를 위해 config에 생성할 config가 읽히지 않도록 하기 위해
 public class ProxyApplication {
 
