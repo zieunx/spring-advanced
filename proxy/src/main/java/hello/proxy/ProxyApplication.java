12d69cf69a2925ced8f7b9604f1064a86bb47b86
zieunx <wldms_1217@naver.com>
2022-02-09 22:59:25 +0900

    feat(JDK동적프록시): 수동 빈 등록

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -10,12 +10,14 @@ import org.springframework.context.annotation.Import;
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 
 // @Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+// @Import(ConcreteProxyConfig.class)
+@Import(DynamicProxyBasicConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의 예제 테스트를 위해 config에 생성할 config가 읽히지 않도록 하기 위해
 public class ProxyApplication {
 
