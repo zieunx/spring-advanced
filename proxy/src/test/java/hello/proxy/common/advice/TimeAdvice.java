0e80ca4466556b4a9d1e2bb936cc79f2c909aaa3
zieunx <wldms_1217@naver.com>
2022-08-30 23:43:03 +0900

    feat(ProxyFactory): 스프링 AOP 모듈(spring-top)의 MethodInterceptor 와 ProxyFactory 를 이용하여 프록시 적용

---
+++ proxy/src/test/java/hello/proxy/common/advice/TimeAdvice.java
@@ -0,0 +1,24 @@
+package hello.proxy.common.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+@Slf4j
+public class TimeAdvice implements MethodInterceptor {
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime =  System.currentTimeMillis();
+
+//        Object result = method.invoke(target, args);
+        Object result = invocation.proceed(); // target 을 알아서 찾아서 리턴해준다.
+
+        long endTime =  System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+
+        log.info("TimeProxy 종료. resultTime={}", resultTime);
+        return result;
+    }
+}
