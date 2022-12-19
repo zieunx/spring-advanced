48e6efa004ec16216d193541b6153b471877326c
zieunx <wldms_1217@naver.com>
2022-12-20 01:19:45 +0900

    feat(aop/실전예제): annotation 기반으로 재시도하기

--- aop/src/main/java/hello/aop/exam/ExamRepository.java
+++ aop/src/main/java/hello/aop/exam/ExamRepository.java
@@ -1,5 +1,6 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Retry;
 import hello.aop.exam.annotation.Trace;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,7 @@ public class ExamRepository {
     private static int seq = 0;
 
     @Trace
+    @Retry(4)
     public String save(String itemId) {
         seq++;
         if (seq % 5 == 0) {
